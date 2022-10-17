927c04a03b716a5b57305e3f61afca0f63799ab3
avocad06 <miggo2704@gmail.com>
2022-10-18 03:26:15 +0900

    function: articles-create

--- Django_Media/uploads/models.py
+++ Django_Media/uploads/models.py
@@ -5,3 +5,4 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
+    image = models.ImageField(upload_to='images/', blank=True)
--- Django_Media/uploads/views.py
+++ Django_Media/uploads/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from .forms import CreateForm
 
 # Create your views here.
 def create(request):
-    return render(request, "uploads/create.html")
+    if request.method == "POST":
+        # 이미지를 받아올 때는 request.FILES 객체도 같이 받아올 것
+        forms = CreateForm(request.POST, request.FILES)
+        if forms.is_valid:
+            forms.save()
+            return render(request, 'base.html')
+    
+    else:
+        forms = CreateForm()
+    context = {
+        "forms" : forms,
+    }
+    return render(request, "uploads/create.html", context)
