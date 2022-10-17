463c38f7300ea424ad7b391d55df094ed79c1f22
avocad06 <miggo2704@gmail.com>
2022-10-17 16:55:41 +0900

    add: MEDIA_URL

--- Django_Media/uploads/models.py
+++ Django_Media/uploads/models.py
@@ -1,3 +1,7 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
--- Django_Media/uploads/urls.py
+++ Django_Media/uploads/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = "uploads"
 
 urlpatterns = [
     path("create/", views.create, name="create"),
-]
+] + static(settings.MEDIA_URL, documnet_root=settings.MEDIA_ROOT)
